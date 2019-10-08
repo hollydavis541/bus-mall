42267b2b5817ec864856a695ef2c9ae0533d0189
Holly Davis <hollydavis541@gmail.com>
2019-10-08 12:00:43 -0700

    removed event listener after rounds complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var rounds = 3;
 var imgDivTag = document.getElementById('div-images');
 var img01Tag = document.getElementById('img01');
 var img02Tag = document.getElementById('img02');
@@ -7,7 +8,6 @@ var img03Tag = document.getElementById('img03');
 
 var totalClicks = 0;
 
-// VIDEO: review video to better understand what null is doing
 var img01OnThePage = null;
 var img02OnThePage = null;
 var img03OnThePage = null;
@@ -15,49 +15,48 @@ var img03OnThePage = null;
 var ProductImage = function(name, imgURL){
   this.name = name;
   this.clicks = 0;
-  // number of times the image appears during the process (I think we will need this in order to calculate percentage)
   this.timesShown = 0;
   this.imgURL = imgURL;
-
+  // array (below) will contain all objects eventually needed
   ProductImage.allImages.push(this);
+};
 
+//TODO: Create working percentage function
+ProductImage.prototype.getPercentOfShown = function(){
+  // this object's total clicks
+  // this object's total shown
+  // format it as a percentage
+  // return (or possibly write it into a property of the object)
+  // this data is available within the array of objects
 };
 
 ProductImage.allImages = [];
 console.log(ProductImage.allImages);
 
-//Helper functions - These are not part of object or the constructor
 var renderNewImages = function(img01Index, img02Index, img03Index){
   img01Tag.src = ProductImage.allImages[img01Index].imgURL;
   img02Tag.src = ProductImage.allImages[img02Index].imgURL;
   img03Tag.src = ProductImage.allImages[img03Index].imgURL;
 };
 
-// Randomly pick new images
 var pickNewImages = function(){
   var img01Index = Math.ceil(Math.random() * ProductImage.allImages.length -1);
-
-  //TODO: Figure out how to add a third item into this cycle
   do {
     var img02Index = Math.ceil(Math.random() * ProductImage.allImages.length-1);
     var img03Index = Math.ceil(Math.random() * ProductImage.allImages.length-1);
   } while(img01Index === img03Index || img01Index === img02Index || img03Index === img02Index);
-
   img01OnThePage = ProductImage.allImages[img01Index];
   img02OnThePage = ProductImage.allImages[img02Index];
   img03OnThePage = ProductImage.allImages[img03Index];
-
   renderNewImages(img01Index, img02Index, img03Index);
 };
 
-// Event Handler
 var handleClickOnImg = function(event){
-
+  var ul = document.getElementById('ul-voteresults');
   //TODO: Change totalClicks to 26 later
-  if(totalClicks < 20) {
-    var thingWeClickedOn = event.target;
-    var id = thingWeClickedOn.id;
-
+  if(totalClicks < rounds) {
+    var imageClicked = event.target;
+    var id = imageClicked.id;
     if(id === 'img01' || id === 'img02' || id === 'img03'){
       if (id === 'img01'){
         img01OnThePage.clicks ++;
@@ -68,18 +67,26 @@ var handleClickOnImg = function(event){
       if (id === 'img03'){
         img03OnThePage.clicks ++;
       }
-
       img01OnThePage.timesShown ++;
       img02OnThePage.timesShown ++;
       img03OnThePage.timesShown ++;
-
       pickNewImages();
     }
   }
   totalClicks ++;
+  if(totalClicks === rounds) {
+    imageClicked.removeEventListener('click', handleClickOnImg);
+    for (var i = 0; i < ProductImage.allImages.length; i++) {
+      var liData = document.createElement('li');
+      liData.textContent = `${ProductImage.allImages[i].name}: ${ProductImage.allImages[i].clicks} total clicks`;
+      ul.appendChild(liData);
+    }
+    console.log(totalClicks);
+  }
 };
 
-imgDivTag.addEventListener('click', handleClickOnImg);
+
+imgDivTag.addEventListener('click', handleClickOnImg); // second parameter is the function above
 
 new ProductImage('R2-D2 Suitcase', './img/bag.jpg');
 new ProductImage('Banana Slicer', './img/banana.jpg');
@@ -93,7 +100,7 @@ new ProductImage('Puppy Duckbill', './img/dog-duck.jpg');
 new ProductImage('Dragon Meat', './img/dragon.jpg');
 new ProductImage('Utensil Pen Cap', './img/pen.jpg');
 new ProductImage('Pet Paw Sweeper', './img/pet-sweep.jpg');
-new ProductImage('Pizza Serving Scissors', './img/sweep.png');
+new ProductImage('Pizza Serving Scissors', './img/scissors.jpg');
 new ProductImage('Shark Sleeping Bag', './img/shark.jpg');
 new ProductImage('Baby Sweeper Onesie', './img/sweep.png');
 new ProductImage('Tauntaun Sleeping Bag', './img/tauntaun.jpg');
